a2253dcc0b93ebdc3a459e175c1038034e85cc07
Georgi <g.velchev12@gmail.com>
2019-08-28 14:47:54 +0300

    add navbar & home page

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -1,1 +1,131 @@
 "use strict";
+
+var controller = {
+  init: function init() {
+    controller.scrollSpy();
+    controller.headerSlider();
+    controller.navbarToggler();
+  },
+  scrollSpy: function scrollSpy() {
+    var sections = {};
+    document.querySelectorAll(".section").forEach(function (e) {
+      sections[e.id] = e.offsetTop;
+    });
+
+    window.onscroll = function () {
+      var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+      for (var elem in sections) {
+        if (sections[elem] <= scrollPosition) {
+          document.querySelector('.active').classList.remove('active');
+          document.querySelector('a[href*=' + elem + ']').classList.add('active');
+        }
+
+        var anchorBtnOffset = document.getElementsByClassName('anchorBtn')[0].offsetTop - 150;
+
+        if (anchorBtnOffset > scrollPosition) {
+          document.getElementById("navbar").style.background = '';
+        } else {
+          document.getElementById("navbar").style.background = 'rgba(0, 0, 0, 0.3)';
+        }
+      }
+    };
+  },
+  headerSlider: function headerSlider() {
+    var carouselSlide = document.querySelector('.slider');
+    var sliderItems = document.querySelectorAll('.testimonial-item');
+    var prevBtn = document.querySelector('#prevBtn');
+    var nextBtn = document.querySelector('#nextBtn');
+    var counter = 1;
+    var size;
+
+    var transformCarousel = function transformCarousel() {
+      carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
+    };
+
+    var sizeOfCarousel = function sizeOfCarousel() {
+      size = carouselSlide.clientWidth;
+      transformCarousel();
+    };
+
+    sizeOfCarousel();
+    transformCarousel();
+
+    var automaticallySlide = function automaticallySlide() {
+      if (counter >= sliderItems.length - 1) {
+        return;
+      }
+
+      carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+      counter++;
+      transformCarousel();
+    };
+
+    nextBtn.addEventListener('click', function () {
+      if (counter >= sliderItems.length - 1) {
+        return;
+      }
+
+      carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+      counter++;
+      transformCarousel();
+    });
+    prevBtn.addEventListener('click', function () {
+      if (counter <= 0) {
+        return;
+      }
+
+      carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+      counter--;
+      transformCarousel();
+    });
+    carouselSlide.addEventListener('transitionend', function () {
+      if (sliderItems[counter].id == 'lastClone') {
+        carouselSlide.style.transition = 'none';
+        counter = sliderItems.length - 2;
+        transformCarousel();
+      }
+
+      if (sliderItems[counter].id == 'firstClone') {
+        carouselSlide.style.transition = 'none';
+        counter = sliderItems.length - counter;
+        transformCarousel();
+      }
+    });
+    window.addEventListener("resize", sizeOfCarousel);
+    window.addEventListener("load", sizeOfCarousel);
+    setInterval(automaticallySlide, 9000);
+  },
+  navbarToggler: function navbarToggler() {
+    if (window.innerWidth <= 945) {
+      var navbarAttrChanging = document.querySelectorAll(".nav-a");
+
+      var toggleNav = function toggleNav(percantage) {
+        document.getElementById("mySidebar").style.width = percantage;
+      };
+
+      document.getElementsByClassName('openbtn')[0].addEventListener('click', function () {
+        document.body.style.overflowY = "hidden";
+        toggleNav("40%");
+      });
+      document.getElementsByClassName('closebtn')[0].addEventListener('click', function () {
+        document.body.style.overflowY = "visible";
+        toggleNav("0");
+      });
+      navbarAttrChanging.forEach(function (e) {
+        e.addEventListener('click', function () {
+          document.body.style.overflowY = "visible";
+          toggleNav("0");
+        });
+      });
+    }
+
+    window.addEventListener("resize", controller.navbarToggler);
+    window.addEventListener("scroll", controller.navbarToggler);
+    window.addEventListener("load", controller.navbarToggler);
+  }
+};
+
+window.onload = function () {
+  controller.init();
+};
