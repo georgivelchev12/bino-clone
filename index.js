61df73706ce529a4fb81e938a28d4927da0bcef2
Georgi <g.velchev12@gmail.com>
2019-08-31 21:52:42 +0300

    set up index and procfile

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var app = express();
+const path = require('path');
 var port = process.env.PORT || 5500;
-
+app.use(express.static(__dirname));
 app.get("/", (req,res) => {
-    res.send("Welcome to bino-clone app");
+    res.sendFile(path.join(__dirname+'/index.html'));
 })
 
 app.listen(port);
