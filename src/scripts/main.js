8e537431e82caa0fd00e7257cea410cfec2fa86c
Georgi <g.velchev12@gmail.com>
2019-09-10 14:54:09 +0300

    add contact-b

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -42,6 +42,7 @@ let controller = {
         window.addEventListener('scroll', changeNavBackground)
         window.addEventListener('load', controller.scrollSpy)
         window.addEventListener('resize', controller.scrollSpy)
+        window.addEventListener('scroll', controller.scrollSpy)
     },
     slider: () => {
         let swiper1 = new Swiper(".slider-1", {
